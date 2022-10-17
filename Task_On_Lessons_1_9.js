79c39171d1465d5f1947e121d45705d5af983060
IslaMSobhy <64289312+Sisco22-maker@users.noreply.github.com>
2022-10-17 08:01:50 +0200

    Add files via upload

---
+++ Task_On_Lessons_1_9.js
@@ -0,0 +1,42 @@
+//The first in the list 
+/*
+in this task:
+
+The first code wont work because the elemnt hasn't been created 
+
+The second will work as it use method called window.onload -> it will wait till the whole page elements loads then it will be excuted
+
+The third code will work as it's place is after the element creation 
+
+*/
+
+//-------------------------------------------//
+
+//The second in the list 
+document.write("<h2> Islam </h2>");
+document.querySelector("h2").style.color = "blue";
+document.querySelector("h2").style.fontSize = "40px";
+document.querySelector("h2").style.fontWeight = "bold";
+document.querySelector("h2").style.textAlign = "center";
+document.querySelector("h2").style.fontFamily = "Arial";
+
+
+
+//---------------------------------------------//
+//The third in the list 
+console.log("%cElzero %cWeb %cSchool","color:red;font-size:40px;font-weight:bold","color:blue;font-size:40px;font-weight:bold","color:green;font-size:40px;font-weight:bold");
+
+//---------------------------------------------//
+
+//The 4th task
+// the time now is 7AM an i can't write the text now maybe later:)
+//--------------------------------------------//
+//The 5th task in the list
+console.table(["islam","sobhy","yousuf","farag","mohamed"]);
+//--------------------------------------------//
+//The 6th task in the list
+//1-> You can use single line comments
+//2-> you can use multi line comments
+console.log("Iam In Console");
+document.write("Iam In Page");
+//---------------------------------------------------
